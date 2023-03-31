011d74919b512d17eb331b3639f559745e3347cc
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-03-31 17:12:30 -0300

    Add home page

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,9 +13,9 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 body{
-  background-color: #333 ;
   background-color: ${(props) => props.theme['gray-900']};
   color: ${(props) => props.theme['gray-300']};
+  -webkit-font-smoothing: antialiased;
 
 }
 
